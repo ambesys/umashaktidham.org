3857f1a7e45e0e0a6c78d31c7632c58c583fe844
Sarthak <mail.ambesys@gmail.com>
2025-12-03 02:27:20 +0000

    deleted from remote

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "PHP Server is working!";
-echo "<br>PHP Version: " . phpversion();
-echo "<br>Current Directory: " . getcwd();
-echo "<br>Files in directory: ";
-print_r(scandir('.'));
-?>
